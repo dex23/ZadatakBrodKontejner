4d414f2f535d1b674ef32cd107fc154bf3290434
vladaB91 <vladabecelic@hotmail.com>
2017-01-20 18:30:28 +0100

    Kreirane klase za zadatke closes #3

---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,28 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by androiddevelopment on 20.1.17..
+ */
+public class Zadatak4BrisanjeVrednosti {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+        }catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                connectionSource.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
